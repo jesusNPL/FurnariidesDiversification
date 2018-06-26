8664dbdc91a9269385e4cdcb38b330a5d71853a9
Jose Eduardo Meireles <jemeireles@gmail.com>
2018-06-26 11:36:57 -0500

    total_richness is now computed as a function of the number of tips in each subtree

--- Scripts/DiversificationRQ_QJ/run_analysis_ddd_multiple_trees.R
+++ Scripts/DiversificationRQ_QJ/run_analysis_ddd_multiple_trees.R
@@ -1,7 +1,23 @@
 source("run_diversification_analyses.R")
+library("ape")
+
 
 path_trees = c("trees/ftree627.nex", "trees/ftree632.nex", "trees/ftree635.nex",
                "trees/ftree641.nex", "trees/ftree768.nex", "trees/ftree912.nex")
 
-lapply(X = path_trees, FUN = run_DDD,
-       total_richness = 685, number_of_trees = 100)
+## Number of tips per subtree
+tree_sizes = sapply(path_trees, function(x){
+    Ntip(read.nexus(x)[[1]])
+})
+
+## Percentage of species sampled per subtree
+sampling_fraction = 0.9
+
+## Observed + Unobserved richness per subclade
+total_richness = tree_sizes + ceiling(tree_sizes * (1 - sampling_fraction) )
+
+
+mapply(run_DDD,
+       tree_file       = path_trees,
+       total_richness  = total_richness,
+       number_of_trees = 100)
