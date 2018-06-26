a1a4a248cdb6e835d6dc9a51202e4ac07a47c6b2
meireles <jemeireles@gmail.com>
2018-06-25 19:30:41 -0500

    updated tree lists

--- Scripts/DiversificationRQ_QJ/run_analysis_paleoenv_sea.R
+++ Scripts/DiversificationRQ_QJ/run_analysis_paleoenv_sea.R
@@ -1,6 +1,7 @@
 source("run_diversification_analyses.R")
 
-path_trees = dir("trees", full.names = TRUE)
+path_trees = c("trees/ftree627.nex", "trees/ftree632.nex", "trees/ftree635.nex",
+               "trees/ftree641.nex", "trees/ftree768.nex", "trees/ftree912.nex")
 
 lapply(X = path_trees, FUN = run_PaleoEnv,
        env_data_file = "./PaleoEnv/PastSeaLevel.txt",
