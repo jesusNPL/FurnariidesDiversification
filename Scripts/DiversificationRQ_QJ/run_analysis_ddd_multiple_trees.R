a9c448e2ffd6361ef2aa428574a0b6fa8a063dc3
Jose Eduardo Meireles <jemeireles@gmail.com>
2018-08-11 10:22:14 -0500

    no table summary

--- Scripts/DiversificationRQ_QJ/run_analysis_ddd_multiple_trees.R
+++ Scripts/DiversificationRQ_QJ/run_analysis_ddd_multiple_trees.R
@@ -2,8 +2,12 @@ source("run_diversification_analyses.R")
 library("ape")
 
 
-path_trees = c("trees/ftree627.nex", "trees/ftree632.nex", "trees/ftree635.nex",
-               "trees/ftree641.nex", "trees/ftree768.nex", "trees/ftree912.nex")
+path_trees = c(#"trees/ftree627.nex",
+               "trees/ftree632.nex",
+               "trees/ftree635.nex",
+               "trees/ftree641.nex",
+               "trees/ftree768.nex",
+               "trees/ftree912.nex")
 
 ## Number of tips per subtree
 tree_sizes = sapply(path_trees, function(x){
@@ -16,8 +20,7 @@ sampling_fraction = 0.9
 ## Observed + Unobserved richness per subclade
 total_richness = tree_sizes + ceiling(tree_sizes * (1 - sampling_fraction) )
 
-
 mapply(run_DDD,
        tree_file       = path_trees,
        total_richness  = total_richness,
-       number_of_trees = 100)
+       number_of_trees = 20)
