b3ab2da52964faf45c35159f7991d81cb77752c6
meireles <jemeireles@gmail.com>
2018-08-16 12:29:59 -0500

    doing DDD trees a few at a time

---
+++ Scripts/DiversificationRQ_QJ/cluster_script/submit_analysis_ddd_multiple_trees_MESABI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#PBS -l walltime=96:00:00,nodes=1:ppn=16,mem=24gb
+#PBS -N j
+#PBS -m abe
+#PBS -M meireles@umn.edu
+module load R/3.3.3
+module load intel
+module load ompi/intel
+
+cd ~/projects/FurDiversification/Scripts/DiversificationRQ_QJ
+
+R --no-save -q < run_analysis_ddd_multiple_trees.R
